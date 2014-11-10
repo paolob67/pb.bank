8fd8362615d7e7a825f9643cc0c25edfdc18c0f9
Paolo Bianchini <paolob_67@yahoo.com>
2014-11-10 17:38:52 +0100

    Added comments

--- adapters/DoubleStepAuthAdapter/DoubleStepAuthAdapter-impl.js
+++ adapters/DoubleStepAuthAdapter/DoubleStepAuthAdapter-impl.js
@@ -1,6 +1,29 @@
+/**
+ * Double Step Authorization Adapter
+ * @author Paolo Bianchini [paolo.bianchini@it.ibm.com]
+ * @copyright IBM Corp. 2014
+ */
 
+/**
+ * @description This adapter accesses a backend db to perform credential checks
+ * @class pb.bank.DoubleStepAuthAdapter
+ * @memberOf pb.bank
+ */
+
+/**
+ * @property {object} userIdentity holds info received from backend
+ * @memberOf pb.bank.DoubleStepAuthAdapter
+ */
 var userIdentity;
 
+/**
+ * @description the login function
+ * @function onAuthRequired
+ * @memberOf pb.bank.DoubleStepAuthAdapter
+ * @param {object} headers
+ * @param {string} errorMessage
+ * @returns {object} data JSON structure indication if authorization is required
+ */
 function onAuthRequired(headers, errorMessage){
 	errorMessage = errorMessage ? errorMessage : null;
 	return {
@@ -10,6 +33,14 @@ function onAuthRequired(headers, errorMessage){
 	};
 }
 
+/**
+ * @description called by the challenge handler to perform the first step of by the submitAuth functions
+ * @function submitAuthStep1
+ * @memberOf pb.bank.DoubleStepAuthAdapter
+ * @param {string} email the userid for the user record
+ * @param {string} password MD5 hash of the password
+ * @returns data JSON structure indication if authorization is required
+ */
 function submitAuthStep1(email, password){
 	
 	var response;
@@ -55,11 +86,18 @@ function submitAuthStep1(email, password){
 	
 }
 
-function submitAuthStep2(answer){
+/**
+ * @description called by the challenge handler to perform the second step of by the submitAuth functions
+ * @function submitAuthStep2
+ * @memberOf pb.bank.DoubleStepAuthAdapter
+ * @param {string} pin the 4 digit pin
+ * @returns {object} data JSON structure indication if authorization is required
+ */
+function submitAuthStep2(pin){
 	
 	// let's check if we got the same pin
 	/** @todo rather than getting this from the userid attributes implement a different pin check method */
-	if (answer == userIdentity.attributes.pin_){
+	if (pin == userIdentity.attributes.pin_){
 		WL.Logger.debug("Step 2 :: SUCCESS");
 		WL.Server.setActiveUser("DoubleStepAuthRealm", userIdentity);
 		WL.Logger.debug("Authorized access granted");
@@ -82,6 +120,11 @@ function getSecretData(){
 	};
 }
 
+/**
+ * @description the logout function
+ * @function onLogout
+ * @memberOf pb.bank.DoubleStepAuthAdapter
+ */
 function onLogout(){
 	userIdentity = null;
 	WL.Server.setActiveUser("DoubleStepAuthRealm", userIdentity);
