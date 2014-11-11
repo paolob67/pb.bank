0ecf18199b01a0981eb605ad6beb0bf0195d41ea
Paolo Bianchini <paolob_67@yahoo.com>
2014-11-11 19:26:27 +0100

    Update view1.js

--- apps/pb_bank_app/common/js/view1.js
+++ apps/pb_bank_app/common/js/view1.js
@@ -11,49 +11,10 @@ angular.module('pb.bank.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [ '$scope', function($scope) {
 	
-	/*
-	$scope.response = "getting data....";
-	
-	$scope.movements = [
-		{
-        	"bala": 14900,
-        	"date": "2014-01-03",
-        	"move": -100
-		},
-		{
-			"bala": 15000,
-			"date": "2014-01-01",
-			"move": 1100
-		},
-		{
-			"bala": 14400,
-			"date": "2014-01-06",
-			"move": -500
-		}
-	];
-		
-	var invocationData = {
-		adapter : the_auth_adapter,
-		procedure: "getSecretData",
-		parameters: []
-	};
-	
-	WL.Client.invokeProcedure(invocationData, {
-		onSuccess: function(_response){
-			$scope.response = JSON.stringify(_response.invocationResult);
-			//window.location = "#/view1";
-			$scope.$apply();
-		}, 
-		onFailure: function (_response) {
-			$scope.response = JSON.stringify(_response.invocationResult);
-			//window.location = "#/view1";
-			$scope.$apply();
-		},
-	});
-	*/
 	
 	$scope.movements = [];
-	var busyInd = new WL.BusyIndicator ("content", {text: "Fetching data..."});
+	
+	// var busyInd = new WL.BusyIndicator ("content", {text: "Fetching data..."});
 
 	busyInd.show();
 
@@ -66,13 +27,13 @@ angular.module('pb.bank.view1', ['ngRoute'])
 
 	WL.Client.invokeProcedure(invocationData, {
 		onSuccess: function(_response){
-			busyInd.hide();
+			// busyInd.hide();
 
 			$scope.movements = _response.invocationResult.movements;
 			$scope.$apply();
 		}, 
 		onFailure: function (_response) {
-			busyInd.hide();
+			// busyInd.hide();
 
 			$scope.movements = JSON.stringify(_response.invocationResult);
 			$scope.$apply();
